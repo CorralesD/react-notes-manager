305e49956d10c21f48f8f1783326ca9e712b749e
Diego Corrales <dcorrales@brooksbell.com>
2024-01-11 12:37:28 -0800

    added fake db and header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header/Header';
 
 export const App = () => {
   return (
     <div>
-      React Redux Starter
+      <Header />
       <Outlet />
     </div>
   );
