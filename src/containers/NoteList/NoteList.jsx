16ec263ad7aa0bfc62cea5218bfe4c8f1607ac99
Diego Corrales <dcorrales@brooksbell.com>
2024-01-24 11:55:19 -0800

    added ability to search

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -1,12 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import s from './style.module.css';
 import { TextCard } from '../../components/TextCard/TextCard';
 import { useNavigate } from 'react-router-dom';
 import { NoteApi } from '../../api/note-api';
 import { deleteNote } from '../../store/notes/notes-slice';
 
-export const NoteList = (props) => {
-  const noteList = useSelector((store) => store.noteSlice.noteList);
+export const NoteList = ({noteList}) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
